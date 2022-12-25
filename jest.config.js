47c1186e30f354f6fc08244df9abed85f2568035
JoseVitor-1337 <josevitordemelo@yahoo.com>
2022-12-25 20:54:06 -0300

    chore: change configs in typescript, jest an ESLint for react

--- jest.config.js
+++ jest.config.js
@@ -5,9 +5,9 @@ module.exports = {
   roots: ["<rootDir>/src"],
   collectCoverageFrom: ["<rootDir>/src/**/*.{ts, tsx}"],
   coverageDirectory: "coverage",
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
   transform: {
-    ".*\\.ts$": "ts-jest",
+    ".*\\.(ts|tsx)$": "ts-jest",
   },
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: "<rootDir>/src/",
