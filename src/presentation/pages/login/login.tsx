672cb65315477afbd969cde64f31281671f8cb8f
JoseVitor-1337 <josevitordemelo@yahoo.com>
2022-12-26 18:48:51 -0300

    refactor: move LoginHeader to a separate componente

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -2,15 +2,12 @@ import React from "react";
 import Styles from "./login.scss";
 
 import { Spinner } from "@presentation/components/spinner/spinner";
-import { Logo } from "@presentation/components/logo/logo";
+import LoginHeader from "@presentation/components/login-header/login-header";
 
 export const Login: React.FC = () => {
   return (
     <div className={Styles.login}>
-      <header className={Styles.header}>
-        <Logo />
-        <h1>4Dev - Enquetes para programadores</h1>
-      </header>
+      <LoginHeader />
 
       <form className={Styles.form}>
         <h2>Login</h2>
