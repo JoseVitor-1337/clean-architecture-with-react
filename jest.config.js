f6da9d068c64a2905fec9ccf1b558a767814f9b1
JoseVitor-1337 <josevitordemelo@yahoo.com>
2022-12-26 00:36:06 -0300

    chore: config sass

--- jest.config.js
+++ jest.config.js
@@ -3,12 +3,13 @@ const { compilerOptions } = require("./tsconfig");
 
 module.exports = {
   roots: ["<rootDir>/src"],
-  collectCoverageFrom: ["<rootDir>/src/**/*.{ts, tsx}"],
+  collectCoverageFrom: ["<rootDir>/src/**/*.{ts, tsx}", "!**/*.d.ts"],
   coverageDirectory: "coverage",
   testEnvironment: "jsdom",
   transform: {
     ".*\\.(ts|tsx)$": "ts-jest",
   },
+  modulePathIgnorePatterns: ["<rootDir>/src/**/*.scss"],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: "<rootDir>/src/",
   }),
