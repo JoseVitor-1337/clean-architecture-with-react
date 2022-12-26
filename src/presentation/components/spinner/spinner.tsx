577ea7a7514f9ef31b4089c0acaeabf018a862a3
JoseVitor-1337 <josevitordemelo@yahoo.com>
2022-12-26 18:42:27 -0300

    feat: add login view

---
+++ src/presentation/components/spinner/spinner.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Styles from "./spinner.scss";
+
+type Props = React.HTMLAttributes<HTMLElement>;
+
+export const Spinner: React.FC<Props> = ({ className, ...rest }) => {
+  return (
+    <div className={[Styles.spinner, className].join(" ")} {...rest}>
+      <div /> <div /> <div /> <div />
+    </div>
+  );
+};
---
+++ src/presentation/pages/login/login.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Styles from "./login.scss";
+
+import LogoSVG from "@assets/images/logo.svg";
+import { Spinner } from "@presentation/components/spinner/spinner";
+
+export const Login: React.FC = () => {
+  return (
+    <div className={Styles.login}>
+      <header className={Styles.header}>
+        <img src={LogoSVG} alt="Logo 4Dev" />
+        <h1>4Dev - Enquetes para programadores</h1>
+      </header>
+
+      <form className={Styles.form}>
+        <h2>Login</h2>
+        <div className={Styles.inputWrap}>
+          <input type="email" name="email" id="email" placeholder="Digite seu email" />
+          <span className={Styles.inputStatus}>🔴</span>
+        </div>
+        <div className={Styles.inputWrap}>
+          <input type="password" name="password" id="password" placeholder="Digite seu email" />
+          <span className={Styles.inputStatus}>🔴</span>
+        </div>
+        <button className={Styles.submit} type="submit">
+          Entrar
+        </button>
+        <span className={Styles.link}>Criar conta</span>
+
+        <div className={Styles.errorWrap}>
+          <Spinner className={Styles.spinner} />
+          <span className={Styles.error}>Error</span>
+        </div>
+      </form>
+
+      <footer className={Styles.footer}></footer>
+    </div>
+  );
+};
