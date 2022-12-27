91e239270f0cfb58b43af249b4c26e8c3eeaba38
JoseVitor-1337 <josevitordemelo@yahoo.com>
2022-12-27 15:05:23 -0300

    test: ensure Login will not render Spinner an Error on initial state

---
+++ src/presentation/pages/login/login.spec.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+import { Login } from "./login";
+
+describe("Login Component", () => {
+  test("Should not render spinner and error in initial state", () => {
+    render(<Login />);
+    const errorWrap = screen.getByTestId("error-wrap");
+    expect(errorWrap.childElementCount).toBe(0);
+  });
+});
