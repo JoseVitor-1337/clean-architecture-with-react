0b3023d930f98c6f28e83a59b3012f9aeaf5f6eb
JoseVitor-1337 <josevitordemelo@yahoo.com>
2022-12-27 17:02:43 -0300

    test: ensure Login calls Validation with correct email

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,13 +1,34 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { cleanup, fireEvent, render, screen } from "@testing-library/react";
+
+import { Validation } from "@presentation/protocols/validation";
 
 import { Login } from "./login";
 
-const makeLoginFactory = (): void => {
-  render(<Login />);
+class ValidationSpy implements Validation {
+  errorMessage: string;
+  input: object;
+
+  validate(input: object): string {
+    this.input = input;
+    return this.errorMessage;
+  }
+}
+
+type MakeLoginFactoryReturn = {
+  validationSpy: ValidationSpy;
+};
+
+const makeLoginFactory = (): MakeLoginFactoryReturn => {
+  const validationSpy = new ValidationSpy();
+  render(<Login validation={validationSpy} />);
+
+  return { validationSpy };
 };
 
 describe("Login Component", () => {
+  afterEach(cleanup);
+
   test("Should not render spinner and error on initial state", () => {
     makeLoginFactory();
     const errorWrap = screen.getByTestId("error-wrap");
@@ -21,9 +42,16 @@ describe("Login Component", () => {
   });
 
   test("Should email and password inputs start with error spans on initial state", () => {
-    render(<Login />);
+    makeLoginFactory();
     const inputErrorSpans = screen.queryAllByTitle("Campo obrigatório");
     expect(inputErrorSpans.length).toBe(2);
     inputErrorSpans.forEach((errorSpan) => expect(errorSpan).toHaveTextContent("🔴"));
+  });
+
+  test("Should call Validation with correct email value", () => {
+    const { validationSpy } = makeLoginFactory();
+    const emailInput = screen.getByTestId("login-email");
+    fireEvent.input(emailInput, { target: { value: "anyEmail" } });
+    expect(validationSpy.input).toEqual({ email: "anyEmail" });
   });
 });
