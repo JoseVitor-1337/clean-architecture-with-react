bb8a93829d71987aa11660eb29426663cabfdb86
JoseVitor-1337 <josevitordemelo@yahoo.com>
2022-12-27 15:44:55 -0300

    refactor: move Login to a factory method

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -3,15 +3,19 @@ import { render, screen } from "@testing-library/react";
 
 import { Login } from "./login";
 
+const makeLoginFactory = (): void => {
+  render(<Login />);
+};
+
 describe("Login Component", () => {
   test("Should not render spinner and error on initial state", () => {
-    render(<Login />);
+    makeLoginFactory();
     const errorWrap = screen.getByTestId("error-wrap");
     expect(errorWrap.childElementCount).toBe(0);
   });
 
   test("Should submit button is disable on initial state", () => {
-    render(<Login />);
+    makeLoginFactory();
     const submitButton = screen.getByTestId("submit");
     expect(submitButton).toBeDisabled();
   });
