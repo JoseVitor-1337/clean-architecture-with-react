f127cbdaf3912e7b8018780d90c5c596d89be46e
JoseVitor-1337 <josevitordemelo@yahoo.com>
2022-12-27 15:40:31 -0300

    fix: add errorMessage on FormStatus span

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -7,13 +7,18 @@ import Styles from "./form-status.scss";
 type Props = {
   isLoading: boolean;
   errorMessage?: string;
+  title?: string;
 };
 
-export const FormStatus: React.FC<Props> = ({ isLoading, errorMessage }) => {
+export const FormStatus: React.FC<Props> = ({ isLoading, errorMessage, title = "Campo obrigatório" }) => {
   return (
     <div data-testid="error-wrap" className={Styles.errorWrap}>
       {isLoading && <Spinner className={Styles.spinner} />}
-      {errorMessage && <span className={Styles.error}>Error</span>}
+      {errorMessage && (
+        <span title={title} className={Styles.error}>
+          {errorMessage}
+        </span>
+      )}
     </div>
   );
 };
