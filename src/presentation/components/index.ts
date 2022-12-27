1da0896ad448b286ce11ddeba10cd1b1faf878a6
JoseVitor-1337 <josevitordemelo@yahoo.com>
2022-12-27 14:52:40 -0300

    fix: remove Router from index in components

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -1,7 +1,6 @@
 export { default as Footer } from "./footer/footer";
 export * from "./form-status/form-status";
 export * from "./input/input";
-export * from "./router/router";
 export { default as LoginHeader } from "./login-header/login-header";
 export * from "./logo/logo";
 export * from "./spinner/spinner";
