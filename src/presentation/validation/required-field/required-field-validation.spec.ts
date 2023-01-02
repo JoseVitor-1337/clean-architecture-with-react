de1da373af1705efb8ffdd5a85442e8c0cba1bcb
JoseVitor-1337 <josevitordemelo@yahoo.com>
2023-01-02 18:01:37 -0300

    refactor: move sut creation to factory method in RequiredFieldValidation tests

--- src/presentation/validation/required-field/required-field-validation.spec.ts
+++ src/presentation/validation/required-field/required-field-validation.spec.ts
@@ -1,15 +1,25 @@
 import { RequiredFieldError } from "../errors";
 import { RequiredFieldValidation } from "./required-field-validation";
 
+type SutReturn = {
+  sut: RequiredFieldValidation;
+};
+
+const makeSut = (): SutReturn => {
+  const sut = new RequiredFieldValidation("email");
+
+  return { sut };
+};
+
 describe("RequiredFieldValidation", () => {
   test("Should return error is field is Empty", () => {
-    const sut = new RequiredFieldValidation("email");
+    const { sut } = makeSut();
     const error = sut.validate("");
     expect(error).toEqual(new RequiredFieldError());
   });
 
   test("Should not return a error if field is not empty", () => {
-    const sut = new RequiredFieldValidation("email");
+    const { sut } = makeSut();
     const error = sut.validate("vitor@gmail.com");
     expect(error).toBeFalsy();
   });
