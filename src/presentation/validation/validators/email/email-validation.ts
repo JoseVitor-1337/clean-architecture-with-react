05e1b4d36725a292f2194e1bce8d2bb73bfef49c
JoseVitor-1337 <josevitordemelo@yahoo.com>
2023-01-02 18:21:34 -0300

    ensure: EmailValidation returns falsy if email is valid

--- src/presentation/validation/validators/email/email-validation.ts
+++ src/presentation/validation/validators/email/email-validation.ts
@@ -4,8 +4,8 @@ import { FieldValidation } from "@presentation/validation/protocols";
 export class EmailValidation implements FieldValidation {
   constructor(readonly field: string) {}
 
-  validate(fieldValue: string): Error {
-    console.log("fieldValue", fieldValue);
-    return new InvalidFieldError(this.field);
+  validate(value: string): Error {
+    const emailRegex = /^\S+@\S+\.\S+$/;
+    return emailRegex.test(value) ? null : new InvalidFieldError(this.field);
   }
 }
