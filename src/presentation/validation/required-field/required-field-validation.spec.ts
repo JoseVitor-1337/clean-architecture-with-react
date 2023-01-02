677bdbf135c771adaf0fcd3c708f38c9a8060687
JoseVitor-1337 <josevitordemelo@yahoo.com>
2023-01-02 17:55:43 -0300

    test: ensure RequiredFieldValidation not return a error if field is not empty

--- src/presentation/validation/required-field/required-field-validation.spec.ts
+++ src/presentation/validation/required-field/required-field-validation.spec.ts
@@ -7,4 +7,10 @@ describe("RequiredFieldValidation", () => {
     const error = sut.validate("");
     expect(error).toEqual(new RequiredFieldError());
   });
+
+  test("Should not return a error if field is not empty", () => {
+    const sut = new RequiredFieldValidation("email");
+    const error = sut.validate("vitor@gmail.com");
+    expect(error).toBeFalsy();
+  });
 });
