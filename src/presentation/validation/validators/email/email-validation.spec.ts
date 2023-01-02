ee3ed9d24a1be3656e3684103091d25364db3926
JoseVitor-1337 <josevitordemelo@yahoo.com>
2023-01-02 18:21:58 -0300

    test: EmailValidation returns falsy if email is valid

--- src/presentation/validation/validators/email/email-validation.spec.ts
+++ src/presentation/validation/validators/email/email-validation.spec.ts
@@ -1,10 +1,18 @@
 import { InvalidFieldError } from "@presentation/validation/errors";
 import { EmailValidation } from "./email-validation";
 
+const makeSut = () => new EmailValidation("email");
+
 describe("Email Validation", () => {
   test("Should return error if email is invalid", () => {
-    const sut = new EmailValidation("email");
+    const sut = makeSut();
     const error = sut.validate("vitor@");
     expect(error).toEqual(new InvalidFieldError("email"));
+  });
+
+  test("Should return falsy if email is valid", () => {
+    const sut = makeSut();
+    const error = sut.validate("josedocarana@gmail.com");
+    expect(error).toBeFalsy();
   });
 });
