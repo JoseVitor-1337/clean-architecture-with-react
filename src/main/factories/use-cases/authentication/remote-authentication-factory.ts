d0f14f21f2dd0c5957926e96052cd557a7f59187
JoseVitor-1337 <josevitordemelo@yahoo.com>
2023-01-03 14:27:46 -0300

    refactor: moce Login factory to their own files

---
+++ src/main/factories/use-cases/authentication/remote-authentication-factory.ts
@@ -0,0 +1,10 @@
+import { RemoteAuthentication } from "@data/use-cases/authentication/remove-authentication";
+import { makeAxiosHtppClient } from "@main/factories/http/axios-http-client-factory";
+import { Authentication } from "@domain/use-cases";
+import { makeApiURL } from "@main/factories/http/api-url-factory";
+
+export const makeRemoteAuthentication = (): Authentication => {
+  const url = makeApiURL();
+  const axiosHttpClient = makeAxiosHtppClient();
+  return new RemoteAuthentication(url, axiosHttpClient);
+};
