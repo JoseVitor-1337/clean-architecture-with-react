69ba017d70c3d91318e8da739c0b4e71226cd3e5
JoseVitor-1337 <josevitordemelo@yahoo.com>
2023-01-24 16:04:55 -0300

    refactor: create sut factory in LocalSaveAccessToken

--- src/data/use-cases/save-access-token/local-save-access-token.spec.ts
+++ src/data/use-cases/save-access-token/local-save-access-token.spec.ts
@@ -1,13 +1,27 @@
 import { SetStorageSpy } from "@data/tests/mock-storage";
 import { LocalSaveAccessToken } from "./local-save-access-token";
 
+type SutTypes = {
+  sut: LocalSaveAccessToken;
+  setStorageSpy: SetStorageSpy;
+};
+
+const makeSut = (): SutTypes => {
+  const setStorageSpy = new SetStorageSpy();
+  const sut = new LocalSaveAccessToken(setStorageSpy);
+
+  return {
+    sut,
+    setStorageSpy,
+  };
+};
+
 describe("LocalSaveAccessToken", () => {
   test("Should call setStorage with correct value", async () => {
-    const setStorage = new SetStorageSpy();
     const accessToken = "TOKEN";
-    const sut = new LocalSaveAccessToken(setStorage);
+    const { sut, setStorageSpy } = makeSut();
     await sut.save(accessToken);
-    expect(setStorage.key).toBe("accessToken");
-    expect(setStorage.value).toBe(accessToken);
+    expect(setStorageSpy.key).toBe("accessToken");
+    expect(setStorageSpy.value).toBe(accessToken);
   });
 });
