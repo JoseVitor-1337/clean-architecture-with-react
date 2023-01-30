b3360c085b8aa82218dfc6ca541dd256121285a7
JoseVitor-1337 <josevitordemelo@yahoo.com>
2023-01-30 15:53:57 -0300

    feat: add LocalStorageAdapter into Login factory

--- src/main/factories/page/login/login-factory.tsx
+++ src/main/factories/page/login/login-factory.tsx
@@ -1,8 +1,10 @@
 import React, { ReactElement } from "react";
+
 import { Login } from "@presentation/pages";
 import { makeRemoteAuthentication } from "@main/factories/use-cases/authentication/remote-authentication-factory";
+import { makeLocalSaveAccessToken } from "@main/factories/use-cases/save-access-token/local-save-access-token-factory";
 import { makeLoginValidation } from "./login-validation-factory";
 
 export const makeLogin = (): ReactElement => {
-  return <Login authentication={makeRemoteAuthentication()} validation={makeLoginValidation()} />;
+  return <Login authentication={makeRemoteAuthentication()} validation={makeLoginValidation()} saveAccessToken={makeLocalSaveAccessToken()} />;
 };
